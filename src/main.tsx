50a215031582096375918ce2f8296a490009f61e
Darren Kent <darrenkent88@gmail.com>
2025-04-17 13:10:34 +0700

    Added Internet Identity Canister + Installed Tailwind and shadcn/ui initialization + edited package scripts

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import { canisterId, idlFactory } from './declarations/backend';
-import './index.scss';
+import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,10 @@
 /// <reference types="vitest" />
+import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 import dotenv from 'dotenv';
 import { defineConfig } from 'vite';
 import environment from 'vite-plugin-environment';
+import path from "path"
 
 dotenv.config();
 
@@ -31,10 +33,12 @@ export default defineConfig({
     react(),
     environment('all', { prefix: 'CANISTER_' }),
     environment('all', { prefix: 'DFX_' }),
+    tailwindcss(),
   ],
-  cacheDir: '../node_modules/.vite',
-  test: {
-    environment: 'jsdom',
-    setupFiles: 'setupTests.ts',
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
   },
+  cacheDir: '../node_modules/.vite',
 });
